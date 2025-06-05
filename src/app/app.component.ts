d8e20d63eef04f0c5baeb399d2ca90cb021f0a97
莊晉瑋 <simon.chuang@iisigroup.com>
2025-06-05 11:21:26 +0800

    feat: release shawn0439

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,30 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'gh-page';
+export class AppComponent implements OnInit {
+  timer: string = '';
+  ngOnInit(): void {
+    this.initTimer();
+    setInterval(() => {
+      this.initTimer();
+    }, 1000);
+  }
+
+  initTimer() {
+    const targetDate = new Date('2025-06-05T16:12:00');
+    const now = new Date();
+    const diff = targetDate.getTime() - now.getTime();
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    this.timer =
+      diff < 0
+        ? '已經出獄了🥳🥳🥳'
+        : `${hours > 9 ? hours : '0' + hours}:${
+            minutes > 9 ? minutes : '0' + minutes
+          }:${seconds > 9 ? seconds : '0' + seconds}`;
+  }
 }
